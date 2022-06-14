ce9663bce65cf9a23f5fc62e3404d64055aede97
Lucas Molinari <lucas.molinari2003@gmail.com>
2022-06-14 19:43:48 -0300

    feitas melhorias na experiencia do usuario e em alguns erros do programa

--- cnpj.py
+++ cnpj.py
@@ -1,6 +1,8 @@
 from random import randint
 
 
+# formato de um cnpj -> xx.xxx.xxx/0001.xx
+
 def formata_cnpj(string):
     cnpj_formatado = ''
     for caracter in string:
@@ -33,7 +35,7 @@ def calcula_total(cnpj):  # 042520110001
     total = 0
     reversos1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
     reversos2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-    if len(cnpj) == reversos1:
+    if len(cnpj) == len(reversos1):
         for i in range(len(cnpj)):
             total += reversos1[i] * int(cnpj[i])
         total = 11 - (total % 11)
@@ -52,14 +54,14 @@ def calcula_total(cnpj):  # 042520110001
 def gera():
     primeiro_digito = randint(0, 9)
     segundo_digito = randint(0, 9)
+    primeiro_bloco = randint(100, 999)
     segundo_bloco = randint(100, 999)
-    terceiro_bloco = randint(100, 999)
-    quarto_bloco = '0001'
+    terceiro_bloco = '0001'
 
-    cnpj_invalidado = f'{primeiro_digito}{segundo_digito}{segundo_bloco}{terceiro_bloco}{quarto_bloco}'
-    validacao_1 = str(calcula_total(cnpj_invalidado))
+    cnpj_invalidado = f'{primeiro_digito}{segundo_digito}{primeiro_bloco}{segundo_bloco}{terceiro_bloco}'
+    validacao_1 = calcula_total(cnpj_invalidado)
     validacao_2 = calcula_total(f'{cnpj_invalidado}{validacao_1}')
 
-    cnpj = f'{primeiro_digito}{segundo_digito}.{segundo_bloco}.{terceiro_bloco}/{quarto_bloco}-' \
+    cnpj = f'{primeiro_digito}{segundo_digito}.{primeiro_bloco}.{segundo_bloco}/{terceiro_bloco}-' \
            f'{validacao_1}{validacao_2}'
     return cnpj
--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import cnpj
 
+option = ""
 if __name__ == "__main__":
-    # cnpj funcional -> 04.252.011/0001-10
-    cnpj_og = input('Insira seu CNPJ ou digite [gerar]: ')
+    # cnpj funcional -> 01.008.365/0001-25
 
-    if cnpj_og.lower() == 'gerar':
-        quantidade = input('Quantos deseja gerar? [Máximo 100]: ')
-        cnpjs = open('cnpjs.txt', 'w')
-        if quantidade.isnumeric():
-            quantidade = int(quantidade)
-            if quantidade <= 100:
-                for i in range(quantidade):
-                    cnpjs.write(cnpj.gera())
-                    cnpjs.write('\n' + '-' * 50 + '\n')
+    while option != "quit":
+        cnpj_og = input('Insira seu CNPJ ou digite [gerar]: ')
+
+        if cnpj_og.lower() == 'gerar':
+            quantidade = input('Quantos deseja gerar? [Máximo 100]: ')
+            cnpjs = open('cnpjs.txt', 'w')
+            if quantidade.isnumeric():
+                quantidade = int(quantidade)
+                if 100 >= quantidade > 0:
+                    for i in range(quantidade):
+                        cnpjs.write(cnpj.gera())
+                        cnpjs.write('\n' + '-' * 50 + '\n')
+                    print('Arquivo .txt criado.')
+                else:
+                    print(f'Não é possível gerar {quantidade} CNPJ')
             else:
-                print(f'Não é possível gerar {quantidade} CNPJ')
-        else:
-            print('Insira apenas números positivos.')
-        print('Arquivo .txt criado.')
-        cnpjs.close()
-    else:
-        teste_cnpj = cnpj.formata_cnpj(cnpj_og)
-        if teste_cnpj.isnumeric():
-            cnpj1 = teste_cnpj[:-2]
-            cnpj1 += str(cnpj.calcula_total(cnpj1))
-            cnpj2 = cnpj1
-            cnpj2 += str(cnpj.calcula_total(cnpj1))
+                print('Insira apenas números positivos.')
 
-            valido = cnpj.valida(teste_cnpj, cnpj2)
-            if valido:
-                print('CNPJ válido.')
+            cnpjs.close()
+            option = input("Digite [quit] ou [continuar] \n")
+        else:
+            teste_cnpj = cnpj.formata_cnpj(cnpj_og)
+            if teste_cnpj.isnumeric():
+                cnpj1 = teste_cnpj[:-2]
+                cnpj1 += str(cnpj.calcula_total(cnpj1))
+                cnpj2 = cnpj1
+                cnpj2 += str(cnpj.calcula_total(cnpj1))
+
+                valido = cnpj.valida(teste_cnpj, cnpj2)
+                if valido:
+                    print('CNPJ válido.')
+                else:
+                    print('CNPJ inválido.')
+
+                option = input("Digite [quit] ou [continue] \n")
+
             else:
-                print('CNPJ inválido.')
-
-        else:
-            print('Insira números ou escreva [gerar].')
+                print('Insira números ou escreva [gerar].')
+                option = input("Digite [quit] ou [continue] \n")
